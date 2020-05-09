91984fcd55da631895adda7719f59d09c8723f12
Abdoulaye KEITA <abdoulk12@hotmail.fr>
2020-05-09 02:59:20 +0200

    Deploy application in raspberry

--- scripts/deploy_application_raspberry.sh
+++ scripts/deploy_application_raspberry.sh
@@ -2,33 +2,27 @@
 
 set -e
 
-if [[ $(arch) == armv7l ]]; then 
-  aplication_name="cv-with-react-run-$(arch)"
-  dockerfile="Dockerfile_run_armv7l"
-else
-  aplication_name="cv-with-react-run"
-  dockerfile="Dockerfile_run"
-fi
+aplication_name="cv-with-react-run-armf"
 
 check_latest_commit_exist(){
   latest_commit=$(git log --format="%h" -n 1)
   check_latest_commit_docker=$(curl -fsS 'https://hub.docker.com/api/audit/v1/build/?include_related=true&offset=0&limit=10&object=%2Fapi%2Frepo%2Fv1%2Frepository%2Fabdoulk12%2Fcv-with-react%2F' | jq ".objects| map (select(.commit|startswith(\"$latest_commit\")))|length")
 }
 
-if [[ $(arch) != armv7l ]]; then
+
+check_latest_commit_exist
+# if the commit id from Docker is the same commit id from git we can download the latest image and use it to deploy our application
+while (( check_latest_commit_docker != 1 )); do
   check_latest_commit_exist
-  # if the commit id from Docker is the same commit id from git we can download the latest image and use it to deploy our application
-  while (( check_latest_commit_docker != 1 )); do
-    check_latest_commit_exist
-    sleep 10;
-    echo "The docker container is not ready yet !"
-  done
+  sleep 10;
+  echo "The docker container is not ready yet !"
+done
+
 echo "The docker container is ready !"
-fi
 
 # Deploy the application (code from git +  react js activated in docker)
 application_version=$(jq -r .version package.json)
-docker build -t "aboulk12/$aplication_name:$application_version" -f "$dockerfile" .
+docker build -t "aboulk12/$aplication_name:$application_version" -f Dockerfile_run_armf .
 [[ $(docker ps -aq -f status=running -f name=$aaplication_name) ]] &&  docker stop "$aplication_name"
 [[ $(docker ps -aq -f status=exited -f name=$aaplication_name) ]] && docker rm "$aplication_name"
 docker run -p 8080:3000 -d "aboulk12/$aplication_name:$application_version" -name "$aplication_name"
@@ -45,7 +39,6 @@ if [[ $latest_commit_message != "Bump from gitlab to version "* ]]; then
   git remote add github git@github.com:abdoulk12/cv-with-react.git
   git fetch --all
 #  git push github
-#  docker push "aboulk12/$aplication_name:$application_version"
 else
   echo "Nothing to do !"
 fi
